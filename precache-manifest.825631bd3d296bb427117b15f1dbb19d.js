21225eec8617e0c3546a72b089a93f846c7ec26b
jaybui <buitanphat26@gmail.com>
2022-06-01 12:52:31 -0500

    Updates

---
+++ precache-manifest.825631bd3d296bb427117b15f1dbb19d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "179d66b2a726e1f80421",
+    "url": "/RobofriendsReact/static/css/main.280347eb.chunk.css"
+  },
+  {
+    "revision": "179d66b2a726e1f80421",
+    "url": "/RobofriendsReact/static/js/main.179d66b2.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/RobofriendsReact/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "fc7042525cf6b711b74d",
+    "url": "/RobofriendsReact/static/css/2.add11d59.chunk.css"
+  },
+  {
+    "revision": "fc7042525cf6b711b74d",
+    "url": "/RobofriendsReact/static/js/2.fc704252.chunk.js"
+  },
+  {
+    "revision": "63a6dd8d0d3ead30a50e",
+    "url": "/RobofriendsReact/static/js/runtime~main.63a6dd8d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RobofriendsReact/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "9f967e2375475b0f4aaa9089b3595ad6",
+    "url": "/RobofriendsReact/index.html"
+  }
+];
